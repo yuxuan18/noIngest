9341b744869ae2890e22b697caec956ba69b8ccf
yuxuan18 <yuxuan18@node0.aqp-comb.joinml-pg0.clemson.cloudlab.us>
2024-04-20 16:03:21 -0400

    udpate vehicle data

--- data/download.sh
+++ data/download.sh
@@ -14,3 +14,15 @@ if [ "$dataset" == "vehicle" ]; then
   echo "done, cleaning up..."
   rm features.tar
 fi
+
+# if the dataset is human
+if [ "$dataset" == "human" ]; then
+  # download the vehicle dataset
+  gdown 16Dw7B-VubtS5M9H4vDGpc7G5Iexu4RFv
+  echo "decompressing the dataset..."
+  unzip features_human.zip
+  ls
+  mv features_human human/features
+  echo "done, cleaning up..."
+  rm features_human.zip
+fi
---
+++ run_vehicle.sh
@@ -0,0 +1,10 @@
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 41,42 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 42,43 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 43,44 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 44,45 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 45,46 --threshold 0.9
+
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 41,42,43 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 41,42,43,44 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 41,42,43,44,45 --threshold 0.9
+screen -d -m python run_noingest.py --data_dir data/vehicle --partition 41,42,43,44,45,46 --threshold 0.9
